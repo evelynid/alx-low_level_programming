33708b232ad4ce3aa371df12b571f9960b353d00
Idehen-Dennis <idehenevelyn3@gmail.com>
2022-09-08 12:05:57 -0700

    Task 5x

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,45 @@
 #include <stdio.h>        
-                                                                                                            
+
+                                                                                                              
+
 /**                                                                                                                                     
-* main - print the string in the printf function                                                                                        
-*
-* Description: using the main function
-* this program prints "with proper grammar, but the outcome is a piece of art,
-* Return: 0
-*/
+ *
+ * * main - print the string in the put function                                                                                           
+ *
+ * *                                                                                                                                       
+ *
+ * * Description: using the main function
+ *
+ * * Return: 0
+ *
+ * */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		char c;
+
+			int i;
+
+				long li;
+
+					long long lli;
+
+						float f;
+
+
+
+							printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+								printf("Size of an int: %ld byte(s)\n", sizeof(i));
+
+									printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+
+										printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+
+											printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+												return (0);
+
 }
