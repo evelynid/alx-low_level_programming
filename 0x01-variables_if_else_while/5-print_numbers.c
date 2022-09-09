022c9eaaf9437522d74721c20e91c46165e0a3e8
Idehen-Dennis <idehenevelyn3@gmail.com>
2022-09-09 04:23:15 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/* prints all single digit numbers of base 10 starting from 0 */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (i = 0; < 10; i++)
+	{
+		printf("%i", i);
+	}
+}
+	printf("\n");
+	return (0);
