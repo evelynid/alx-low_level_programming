c4932e92ca66b7715f459e402b5b73e31543e50a
Idehen-Dennis <idehenevelyn3@gmail.com>
2022-09-09 02:23:14 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <time.h>
+/* assign a random number to the variable n each time it is executed */
+
+/* store a different value every time this program is runned */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >  0 = +1)
+		(n = 0 = 0)
+			(n < 0 = -1)
+	return (0);
+}
